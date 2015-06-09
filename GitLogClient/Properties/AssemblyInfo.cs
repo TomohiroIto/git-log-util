08c5e84a9399fab74c3dc0e2b08e46afb1af2254
Tomohiro Ito <ito.tomohiro+github@gmail.com>
2015-06-09 16:01:21 +0900

    Small changes in project settings

--- GitLogClient/Properties/AssemblyInfo.cs
+++ GitLogClient/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("GitLogClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Tomohiro Ito")]
 [assembly: AssemblyProduct("GitLogClient")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © Tomohiro Ito 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("069b4357-de01-4e42-bff6-9b468faeaade")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
